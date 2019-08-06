82dc6c2a1f6486ae367d6d69e57a4f6d59785c33
kpilszak <klaudia.pilszak@gmail.com>
2019-08-06 20:21:45 +0200

    Create empty array

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class BinarySearch {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Binary Search Algorithm");
+
+        System.out.println("Enter total number of elements:");
+        int lengthOfTheArray = input.nextInt();
+
+        int[] array = new int[lengthOfTheArray];
+    }
 }
