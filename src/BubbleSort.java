3e8430f50851132eb18f43bc3100225b1e59829f
kpilszak <klaudia.pilszak@gmail.com>
2019-09-07 19:59:52 +0200

    Implement main in BubbleSort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,2 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bubble Sort Algorithm");
+
+        System.out.println("Enter total number of elements:");
+        int lengthOfTheArray = input.nextInt();
+
+        int[] arrayOfIntegersToSort = new int[lengthOfTheArray];
+
+        System.out.printf("Enter %d integers:%n", lengthOfTheArray);
+        for (int i = 0; i < lengthOfTheArray; i++) {
+            arrayOfIntegersToSort[i] = input.nextInt();
+        }
+
+        int[] sortedArray = bubbleSort(arrayOfIntegersToSort);
+
+        System.out.printf("Sorted array using Bubble Sort Algorithm: %s%n",
+                Arrays.toString(sortedArray));
+
+        input.close();
+    }
 }
