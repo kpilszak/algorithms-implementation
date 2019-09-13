ae91beb4f84fbe91379e0210c0520ab1ca48aa20
kpilszak <klaudia.pilszak@gmail.com>
2019-09-13 09:48:14 +0200

    Implement main

--- src/Quicksort.java
+++ src/Quicksort.java
@@ -1,2 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Quicksort {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Quicksort Algorithm");
+
+        System.out.println("Enter total number of elements:");
+        int lengthOfTheArray = input.nextInt();
+
+        int[] arrayOfIntegersToSort = new int[lengthOfTheArray];
+
+        System.out.printf("Enter %d integers:%n", lengthOfTheArray);
+        for (int i = 0; i < lengthOfTheArray; i++) {
+            arrayOfIntegersToSort[i] = input.nextInt();
+        }
+
+        int[] sortedArray = quicksort(arrayOfIntegersToSort);
+
+        System.out.printf("Sorted array using Quicksort Algorithm: %s%n",
+                Arrays.toString(sortedArray));
+
+        input.close();
+    }
 }
