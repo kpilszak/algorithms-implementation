19dce7ed8f834920c860f0e14c34c6ea597db965
kpilszak <klaudia.pilszak@gmail.com>
2019-10-01 07:06:21 +0200

    Implement main

--- src/CountingSort.java
+++ src/CountingSort.java
@@ -1,2 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class CountingSort {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Counting Sort Algorithm");
+
+        System.out.println("Enter total number of elements:");
+        int lengthOfTheArray = input.nextInt();
+
+        int[] arrayOfIntegersToSort = new int[lengthOfTheArray];
+
+        System.out.printf("Enter %d integers:%n", lengthOfTheArray);
+        for (int i = 0; i < lengthOfTheArray; i++) {
+            arrayOfIntegersToSort[i] = input.nextInt();
+        }
+
+        int[] sortedArray = countingSort(arrayOfIntegersToSort);
+
+        System.out.printf("Sorted array using Counting Sort Algorithm: %s%n",
+                Arrays.toString(sortedArray));
+
+        input.close();
+    }
 }
