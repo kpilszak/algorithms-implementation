14313d02e4358e454917cc672348925df067b829
kpilszak <klaudia.pilszak@gmail.com>
2019-12-23 06:49:36 +0100

    Implement main in mergesort

--- src/Mergesort.java
+++ src/Mergesort.java
@@ -1,2 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Mergesort {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Mergesort Algorithm");
+
+        System.out.println("Enter total number of elements:");
+        int lengthOfTheArray = input.nextInt();
+
+        int[] arrayOfIntegersToSort = new int[lengthOfTheArray];
+
+        System.out.printf("Enter %d integers:%n", lengthOfTheArray);
+        for (int i = 0; i < lengthOfTheArray; i++) {
+            arrayOfIntegersToSort[i] = input.nextInt();
+        }
+
+        int[] sortedArray = mergesort(arrayOfIntegersToSort);
+
+        System.out.printf("Sorted array using Mergesort Algorithm: %s%n",
+                Arrays.toString(sortedArray));
+
+        input.close();
+    }
 }
